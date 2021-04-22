03df63ce7077164db9d5d4d3a23e220492aa624d
Alp3n <adam@walukiewicz.eu>
2021-04-22 11:24:52 +0700

    changes updated balancing from main

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,4 +1,10 @@
-const URL_IP = window.location.hostname.endsWith('.wmes.pl') ? '' : 'https://dev.wmes.pl';
+// const URL_IP = `http://192.168.1.100`;
+// const URL_IP = `http://192.168.1.101`;
+// const URL_IP = `http://192.168.1.102`;
+// const URL_IP = `http://192.168.1.103`;
+// const URL_IP = `http://172.20.10.8`;
+const URL_IP = `https://dev.wmes.pl`;
+
 
 export const URL_CHECK_LOGIN = `${URL_IP}/users/self`;
 export const URL_LOGIN = `${URL_IP}/login`;
